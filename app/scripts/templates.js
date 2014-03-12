6e25fbc3b28bba4f3bf1e7ce17548a4269bef2df
liz rush <liz@lizmrush.com>
2014-03-12 14:55:40 -0700

    adds cart link on header

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -1,10 +1,18 @@
 Ember.TEMPLATES["application"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  var buffer = '', stack1, helper, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  var buffer = '', stack1, helper, options, self=this, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
 
+function program1(depth0,data) {
+  
+  
+  data.buffer.push("Cart");
+  }
 
-  data.buffer.push("<div class='container'>\n  <h1>kittyimages</h1>\n  <p class='lead'>Your one stop shop for images of cats.</p>\n\n\n  <hr>\n\n  ");
+  data.buffer.push("<div class='container'>\n  <h1>kittyimages</h1>\n  <p class='lead'>Your one stop shop for images of cats.</p>\n  ");
+  stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],data:data},helper ? helper.call(depth0, "cart", options) : helperMissing.call(depth0, "link-to", "cart", options));
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n\n\n\n  <hr>\n\n  ");
   stack1 = helpers._triageMustache.call(depth0, "outlet", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("\n\n  ");
