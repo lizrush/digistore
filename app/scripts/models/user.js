2a7bc6ebed82ee6e21c514e645e2dc8c91d7854f
liz rush <liz@lizmrush.com>
2014-03-17 16:17:08 -0700

    update user model fixtures

--- app/scripts/models/user.js
+++ app/scripts/models/user.js
@@ -1,11 +1,11 @@
 App.User = DS.Model.extend({
   customer_name: DS.attr('string'),
   email: DS.attr('string'),
-  status: DS.attr('string'),
   zipcode: DS.attr('string'),
   expdate: DS.attr('date'),
   ccv: DS.attr('string'),
-  ccnumber: DS.attr('string')
+  ccnumber: DS.attr('string'),
+  admin: DS.attr('boolean')
 });
 
 App.Order.FIXTURES = [
@@ -13,10 +13,21 @@ App.Order.FIXTURES = [
     id: 1,
     customer_name: "Liz Rush",
     email: 'liz.m.rush@gmail.com',
-    status: 'pending',
     zipcode: '98112',
     expdate: '',
     ccv: '123',
-    ccnumber: '1234567890123456'
+    ccnumber: '1234567890123456',
+    admin: true
+  },
+
+  {
+    id: 2,
+    customer_name: "non-admin user",
+    email: 'liz.m.rush+admin@gmail.com',
+    zipcode: '98112',
+    expdate: '',
+    ccv: '123',
+    ccnumber: '1234567890123456',
+    admin: true
   }
 ]
