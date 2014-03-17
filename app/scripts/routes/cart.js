e7cf68becb3a0735d114bab7ac6ed269d40b0bb3
liz rush <liz@lizmrush.com>
2014-03-17 13:25:51 -0700

    updates remove button

--- app/scripts/routes/cart.js
+++ app/scripts/routes/cart.js
@@ -1,13 +1,13 @@
 App.CartRoute = Ember.Route.extend({
 
   model: function (params) {
-    return this.store.find("cart", localStorage.cartId);
+    return this.store.find('cart', localStorage.cartId);
   },
 
 
   actions: {
     deleteItem: function(item){
-      this.store.find('cart', 1).then(function(cart){
+      this.store.find('cart', localStorage.cartId).then(function(cart){
         cart.get('items').then(function(items){
           items.removeObject(item);
         });
