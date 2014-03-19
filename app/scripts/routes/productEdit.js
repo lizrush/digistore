2be04661b6c670cdba37f1ffa31fd6abc100397b
liz rush <liz@lizmrush.com>
2014-03-18 19:00:58 -0700

    productEdit

---
+++ app/scripts/routes/productEdit.js
@@ -0,0 +1,11 @@
+App.ProductEditRoute = Ember.Route.extend({
+  model: function (params) {
+    return this.store.find('product', params.product_id);
+  },
+
+  actions: {
+    saveProduct: function (product) {
+      product.save();
+    }
+  }
+});
