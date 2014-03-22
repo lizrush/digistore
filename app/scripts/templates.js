be37c54c76b319d823ebb5e9a1bf7a9aece7014a
liz rush <liz@lizmrush.com>
2014-03-21 22:31:51 -0700

    attempt to prep for deployment

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -224,12 +224,7 @@ function program5(depth0,data) {
   data.buffer.push(" Cancel ");
   }
 
-  stack1 = helpers._triageMustache.call(depth0, "model", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
-  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("\n");
-  stack1 = helpers._triageMustache.call(depth0, "controller", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
-  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("\n\n<div class='row'>\n  <div class='col-md-6'>\n    <table class=\"table table-hover\">\n      <thead>\n        <th>Product</th>\n        <th>Quantity</th>\n        <th>Price</th>\n        <th>Subtotal</th>\n        <th></th>\n      </thead>\n      <tbody>\n      ");
+  data.buffer.push("<div class='row'>\n  <div class='col-md-6'>\n    <table class=\"table table-hover\">\n      <thead>\n        <th>Product</th>\n        <th>Quantity</th>\n        <th>Price</th>\n        <th>Subtotal</th>\n        <th></th>\n      </thead>\n      <tbody>\n      ");
   stack1 = helpers.each.call(depth0, "item", "in", "content", {hash:{
     'itemController': ("item")
   },hashTypes:{'itemController': "STRING"},hashContexts:{'itemController': depth0},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0,depth0,depth0],types:["ID","ID","ID"],data:data});
