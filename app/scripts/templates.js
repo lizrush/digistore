a4b0b31d0027a8a73988d39bb858449c2d7449d9
liz rush <liz@lizmrush.com>
2014-03-23 16:09:20 -0700

    manage orders route

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -280,7 +280,7 @@ function program5(depth0,data) {
   
 });
 
-Ember.TEMPLATES["manageOrder"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+Ember.TEMPLATES["manageOrders"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1, self=this, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
